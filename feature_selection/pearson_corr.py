0e3ce49b16c84eefc24e4334753aba59de36d32e
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2023-04-06 23:02:59 -0400

    update dataset, add scripts

---
+++ feature_selection/pearson_corr.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+if __name__ == "__main__":
+    df = pd.read_csv("DontGetKicked/training.csv")
+    corr = df.corr(numeric_only=True, method="pearson")
+    mask = np.triu(np.ones_like(corr))
+    sns.heatmap(corr, annot=True, mask=mask)
+    plt.show()
